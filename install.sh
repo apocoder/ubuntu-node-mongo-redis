000b69151927bcfead60e858b30064de7a3abf9d
Abdullah Eroglu <apocoder@gmail.com>
2020-09-08 16:38:04 +0300

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+sudo apt update
+curl -sL https://deb.nodesource.com/setup_14.x | sudo bash -
+sudo apt -y install nodejs
+node  -v
+sudo apt -y install gcc g++ make
+
+sudo apt update
+sudo apt install mongodb-org
+sudo apt install mongodb-org=4.2.8 mongodb-org-server=4.2.8 mongodb-org-shell=4.2.8 mongodb-org-mongos=4.2.8 mongodb-org-tools=4.2.8
+sudo systemctl enable mongod
+sudo systemctl start mongod
+
+sudo apt update
+sudo apt install redis-server
+#Change redis supervisord:no with systemd
+#sudo nano /etc/redis/redis.conf
+
