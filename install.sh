6330d6eee388ef6aeca3b5c9609b3fef04647c90
Abdullah Eroglu <apocoder@gmail.com>
2024-03-01 13:08:04 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo apt update
-curl -sL https://deb.nodesource.com/setup_17.x | sudo bash -
+curl -sL https://deb.nodesource.com/setup_18.x | sudo bash -
 sudo apt -y install nodejs
 node  -v
 sudo apt -y install gcc g++ make
